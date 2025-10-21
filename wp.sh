2b71e7874d0a2d65b7912ac7bf2a0cab02c6a5ba
Ryan Kraay <rk.no-reply@kurses.com>
2025-10-21 10:07:32 +0200

    More tests

---
+++ wp.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+bats_format_file_line_reference_custom() {
+   printf -v "$output" "%s:L%d" "$1" "$2"
+}
+export -f bats_format_file_line_reference_custom
+
+PWD=.
+TERM=vt100 $PWD/example.bats --formatter pretty --report-formatter junit -o $PWD/out  --line-reference-format custom
